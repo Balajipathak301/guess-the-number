567f985c66231341fec14dfe040b2018e801fb71
Balajipathak301 <balajipathak231@gmail.com>
2025-05-14 17:50:59 +0530

    Add files via upload

---
+++ guess the number/guess the number game.py
@@ -0,0 +1,20 @@
+"""hidden numbers
+can only take 9 gusses
+if wrong game over
+if correct game over
+number between 1-100"""
+print("enter a number between 1-100")
+n=0
+while (n<9):
+    i=int(input())
+    if i==18:
+        print("number was 18 , You won")
+        print("game over")
+    elif i>18:
+        print("try a smaller number")
+    else:
+        print("try a greater number")
+    n=n+1
+if n>8:
+    print("chances over , You lost")
+    print("game over1")
